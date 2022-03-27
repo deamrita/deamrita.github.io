f0d7dc3d01a76df8c3d67fca07911c473f409a3c
Amrita De <deamrita2015@gmail.com>
2022-03-27 20:50:27 +0530

    March Update

--- js/hover_rounak.js
+++ js/hover_rounak.js
@@ -2,22 +2,22 @@
 	'use strict';
 
     let work_data = {
-        "Python" : {"link":["https://github.com/deamrita/Investigating-Netflix-Movies","https://github.com/deamrita/App-Market-Analysis-on-PlayStore"],
-                    "image_urls":["images/nf.jpg","images/playstore.jpg"],
-                    "a_links_texts":["Invesgating Netflix Movies","App Market Analysis on PlayStore"],
-                    "project_span_texts":["Applying the foundational Python skills by manipulating and visualizing movie and TV data.",
-                    "Load, clean, and visualize scraped Google Play Store data to gain insights into the Android app market."]
+        "Python" : {"link":["https://github.com/deamrita/customer_segmentation","https://github.com/deamrita/AB_testing_ecom_website"],
+                    "image_urls":["images/p1.jpg","images/p2.jpg"],
+                    "a_links_texts":["Customer segmentation of a supermarket using K means clustering","AB testing for a new version of e-commerce site"],
+                    "project_span_texts":["Build an unsupervised model to segment the customers of a popular supermarket store based on their RFM score.",
+                    "Ran an A/B testing in Jupyter Notebook to check if the new version of an e-commerce website will perform better or worse in terms of conversion rate."]
                     },
-        "R" : {"link":["https://www.r-project.org/"],
-        "image_urls":["images/coming soon.jpeg"],
-        "a_links_texts":["I am Still exploring and learning R"],
-        "project_span_texts":["Feel free to suggest me some interesting projects on R"]
-        },
-        "Tableau" : {"link":["https://public.tableau.com/app/profile/amrita.de7431/viz/ICCT20WC_DataAnalysis/Dashboard1","https://public.tableau.com/app/profile/amrita.de7431/viz/CovidDashboardOct21/Covid19Dashboardworldwide"],
-        "image_urls":["images/t1.png","images/t2.png"],
-        "a_links_texts":["ICCT20WC_DataAnalysis","Covid Dashboard Oct21"],
-        "project_span_texts":["As the ICC Men's T20 World Cup 2021 has been started, here is a Tableau Visualization of records from past seasons (2007 to 2016) including different viz. like Top Batters with most runs, Best Strike Rates, most no of 6's, players with most no. of catches.",
-        "Worldwide Covid19 Data Viz for Oct2021."]
+        // "R" : {"link":["https://www.r-project.org/"],
+        // "image_urls":["images/coming soon.jpeg"],
+        // "a_links_texts":["I am Still exploring and learning R"],
+        // "project_span_texts":["Feel free to suggest me some interesting projects on R"]
+        // },
+        "Tableau" : {"link":["https://public.tableau.com/app/profile/amrita.de7431/viz/IIM_cal_casestudy_HRAnalytics/Story1","https://public.tableau.com/app/profile/amrita.de7431/viz/TableauSpotify_16388657936010/Dashboard1"],
+        "image_urls":["images/HR.png","images/sf.png"],
+        "a_links_texts":["HR Analytics Dashboard","Top 100 songs on Spotify"],
+        "project_span_texts":["Three interactive dashboards in Tableau to deliver key insights into diversity profile, the satisfaction of employees, recruiting sources & pay and attrition.",
+        "Top 100 played songs on Spotify"]
         },
         "SQL" :{"link":["https://github.com/deamrita/ICC-T20-WC-Data-Analysis","https://github.com/deamrita/SQLPortfolioProject1"],
         "image_urls":["images/cricket.jpeg","images/covid.jpeg"],
